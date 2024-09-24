4966f5136b225938b2ba9e15028d94e2dc519273
gachu2002 <lethanhnguyenak29@gmail.com>
2024-09-24 13:36:13 +0700

    Fin JS Ex3 Reverse a String

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let newString = '';
+    for(let i = string.length - 1; i >= 0; i--){
+        newString += string[i];
+    }
+    return newString;
 };
+/*Solution 2
+const reverseString = function(string) {
+    return string.split('').reverse().join('');
+};
+*/
 
 // Do not edit below this line
 module.exports = reverseString;
