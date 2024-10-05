7e1445e08d0a657d0af330e8699b39563f4a30f4
gachu2002 <lethanhnguyenak29@gmail.com>
2024-10-05 21:13:06 +0700

    Finish JS exercises

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+    a = parseInt(a);
+    if (a < 0) {
+        return "OOPS";
+    } else if (a <= 1) {
+        return a;
+    } else {
+        return fibonacci(a - 1) + fibonacci(a - 2);
+    }
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    people.sort((a, b) => {
+        if (!b.yearOfDeath) {
+            b.yearOfDeath = new Date().getFullYear()
+        }
+        return (a.yearOfDeath - a.yearOfBirth) - (b.yearOfDeath - b.yearOfBirth)
+    })
+    return people[people.length - 1]
 };
 
 // Do not edit below this line
